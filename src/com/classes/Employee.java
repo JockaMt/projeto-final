67ea51df46e74d9a8706072b26edf7731c5a8f6b
JairontSantos <jairon.tavares@hotmail.com>
2023-09-16 10:56:48 -0300

    Person/Emplyee

---
+++ src/com/classes/Employee.java
@@ -0,0 +1,19 @@
+package com.classes;
+
+public class Employee extends Person {
+    
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+	
+	public Double wage;
+	public String role;
+}
---
+++ src/com/classes/Person.java
@@ -0,0 +1,19 @@
+package com.classes;
+
+public class Person {
+    public String name;
+    public String sex;
+    public int age;
+    
+	public void setName (String newName) {
+		this.name = newName;
+	}
+	
+	public void setSex (String newSex) {
+		this.sex = newSex;
+	}
+	
+	public void setAge (int newAge) {
+		this.age = newAge;
+	}
+}
