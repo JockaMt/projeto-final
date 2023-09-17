663602336e0d787c1e334324432362a7fb2d98e5
jockaplay <jocksonmatheus@gmail.com>
2023-09-17 13:16:31 -0300

    backup

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
-import com.type.Alimentos;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import javax.swing.*;
 
 public class Main {
-    static Alimentos a0 = new Alimentos(0, "Arroz", 5.25, 500);
     public static void main (String[] args){
-        System.out.println(a0.getQuantidade());
+        JFrame janela = new JFrame("Hotéis");
+        janela.getContentPane().setLayout(new FlowLayout());
+        
+        JButton a = new JButton("Adicionar hotel");
+        a.setBackground(new Color(255, 255, 255, 255));
+        a.setBorderPainted(false);
+        a.setFocusPainted(false);
+        a.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        janela.getContentPane().add(a);
+
+        janela.setSize(400, 200);
+        janela.setVisible(true);
     }
 }
