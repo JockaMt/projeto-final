589d2c8d39c386e319fbadaadc0f09fb50d583f9
jockaplay <jocksonmatheus@gmail.com>
2023-09-18 01:10:12 -0300

    01:10 change

--- src/com/classes/Cliente.java
+++ src/com/classes/Cliente.java
@@ -1,5 +1,18 @@
 package com.classes;
 
 public class Cliente extends Person {
+    String type = "Normal";
+
+    public Cliente(String name, String sex, int age, String type) {
+        super(name, sex, age);
+        setType(type);
+    }
     
+    private void setType (String type) {
+        this.type = type;
+    }
+    public String getType () {
+        // Cliente normal, vip, permuta, etc...
+        return this.type;
+    }
 }
