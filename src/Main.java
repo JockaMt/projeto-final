592696e57d1a5f65b68824257cd0df29a678a00c
jockaplay <jocksonmatheus@gmail.com>
2023-09-17 21:49:15 -0300

    add hotel id

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-
+import com.Hotel;
 
 public class Main {
 	public static void main(String[] args) {
+		Hotel h1 = new Hotel();		
+		Hotel h2 = new Hotel();
+		Hotel h3 = new Hotel();
+
+		System.out.println(h1.getID());		
+		System.out.println(h2.getID());
+		System.out.println(h3.getID());
+
 		}
 	}
     
--- src/com/Hotel.java
+++ src/com/Hotel.java
@@ -1,5 +1,12 @@
 package com;
 
 public class Hotel {
-    
+    private static int ID = 0;
+    public Hotel () {
+        ID ++;
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
