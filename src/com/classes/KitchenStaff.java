e0f576453690b5423eae224d6d63824deb2444b2
jockaplay <jocksonmatheus@gmail.com>
2023-09-19 18:58:56 -0300

    + funções vazias

--- src/com/classes/KitchenStaff.java
+++ src/com/classes/KitchenStaff.java
@@ -4,4 +4,9 @@ public class KitchenStaff extends Employee {
     public KitchenStaff(String name, String sex, int age, String role, Double wage) {
         super(name, sex, age, role, wage);
     }
+
+    // Só pra ter um método na classe.
+    public void cook() {
+        System.out.println("Cozinhando algo...");
+    }
 }
--- src/com/classes/Room.java
+++ src/com/classes/Room.java
@@ -2,7 +2,8 @@ package com.classes;
 
 public class Room {
     private static int count = 0;
-    public int id = 1;
+    private int id = 1;
+    private Cliente[] clientes = new Cliente[4];
 
     public Room () {
         id += count;
@@ -11,5 +12,13 @@ public class Room {
 
     public int getID() {
         return id;
+    }
+
+    public void addCliente(int slot, Cliente cliente) {
+        clientes[slot] = cliente;
+    }
+
+    public void removeClient(int slot) {
+        clientes[slot] = null;
     }
 }
