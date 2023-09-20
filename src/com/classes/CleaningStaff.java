048891d5ee94e177251d2283066aa9a9a6862a12
JairontSantos <jairon.tavares@hotmail.com>
2023-09-19 21:12:48 -0300

    menu start

--- src/com/classes/CleaningStaff.java
+++ src/com/classes/CleaningStaff.java
@@ -3,9 +3,11 @@ package com.classes;
 public class CleaningStaff extends Employee {
     public CleaningStaff(String name, String sex, int age, String role, Double wage) {
         super(name, sex, age, role, wage);
+        
     }
 
     public void clearRoom(int room) {
         System.out.println("Limpando a sala " + room);
     }
+
 }
