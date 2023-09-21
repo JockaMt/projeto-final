5cbae7070198d5cf384165c99b227a5b1fd8af8a
jockaplay <jocksonmatheus@gmail.com>
2023-09-20 23:52:09 -0300

    add code at Hotel.java

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,52 @@
-import com.classes.CleaningStaff;
-import com.classes.Employee;
+import com.Hotel;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
+		ArrayList<Hotel> hoteis = new ArrayList<Hotel>();
 		Scanner scanner = new Scanner(System.in);
 		int start;
 
         do {
 			System.out.println("=======================================================================");
-			System.out.println("Bem Vindo! Você está entrando ao Hall de comando do Hotel JOCKASPLAYS!");
+			System.out.println("Bem Vindo! Você está entrando ao gerenciador de Hoteis!");
 			System.out.println("=======================================================================");
 			System.out.println("Escolha as opções de comando a seguir!");
 			System.out.println("=======================================================================");
             System.out.println("Selecione uma opção:");
-            System.out.println("1. Criar ID para funcionário novo");
-            System.out.println("2. Comandar funções de Funcionários");
-			System.out.println("3. Ver os funcionários do sistema");
-			System.out.println("4. Registrar um novo cliente em alguns dos quartos");
-            System.out.println("5. Sair");
+            System.out.println("1. Criar novo hotel");
+            System.out.println("2. Deletar hoteis");
+			System.out.println("3. Ver hoteis");
+            System.out.println("4. Sair");
 			System.out.println("=======================================================================");
 
             start = scanner.nextInt();
 
             switch (start) {
                 case 1:
-                    System.out.println("Você irá criar uma nova ID de Funcionário!");
-					System.out.println("=======================================================================");
+                    System.out.println("Você irá criar um novo hotel!");
                     break;
                 case 2:
-                    System.out.println("Você irá designar funções para funcionários");
-					System.out.println("=======================================================================");
+                    System.out.println("Você irá deletar um hotel!");
                     break;
 				case 3:
-					System.out.println("Irá ver os funcionários a seguir");
-					System.out.println("=======================================================================");
+					System.out.println("Irá ver todos os hoteis!");
 					break;
                 case 4:
-                    System.out.println("Colocar um novo cliente no sistema de quartos");
+                    System.out.println("Saindo do programa");
 					System.out.println("=======================================================================");
                     break;
-				case 5:
-					System.out.println("O programa irá encerrar");
-					System.out.println("=======================================================================");
-					break;
-                default:
+					default:
                     System.out.println("Opção inválida. Tente novamente.");
-					System.out.println("=======================================================================");
                     break;
             }
 
-        } while (start != 5);
+        } while (start != 4);
 
+		Hotel h1 = new Hotel();
+		h1.addEmployee();
         scanner.close();
 	}
 }
--- src/com/Hotel.java
+++ src/com/Hotel.java
@@ -1,15 +1,18 @@
 package com;
 import java.util.ArrayList;
+import java.util.Scanner;
 import com.classes.Employee;
 import com.classes.Cliente;
 
 public class Hotel {
+    Scanner scanner = new Scanner(System.in);
     private static int count = 0;
-    public int id = 1;
+    private int id = 1;
+    private Boolean open = false; 
 
 	//Iniciando uma lista de empregados.
-	ArrayList<Employee> empregados = new ArrayList<Employee>();
-	ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	private ArrayList<Employee> empregados = new ArrayList<Employee>();
+	private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
     public Hotel () {
         id += count;
@@ -21,12 +24,36 @@ public class Hotel {
     }
 
     public void addEmployee() {
-        for (int i = 0; i < empregados.size(); i++){
-            
-        }
+        int start;
+        do {
+            System.out.println("=======================================================================");
+			System.out.println("Bem Vindo! Você está entrando ao Hall de comando do Hotel " + getID());
+			System.out.println("=======================================================================");
+			System.out.println("Escolha as opções de comando a seguir!");
+			System.out.println("=======================================================================");
+            System.out.println("Selecione uma opção:");
+            System.out.println("1. Criar funcionário novo");
+            System.out.println("2. Comandar funções de Funcionários");
+			System.out.println("3. Ver os funcionários do sistema");
+			System.out.println("4. Registrar um novo quartos");
+			System.out.println("5. Registrar um novo cliente em alguns dos quartos");
+            System.out.println("6. Sair");
+			System.out.println("=======================================================================");
+            start = scanner.nextInt();
+            switch (start) {
+                case 6:
+                    break;
+                default:
+                    break;
+            }
+        } while (start != 6);
     }
 
     public void addCliente() {
-        
+        if (open) {
+
+        } else {
+            System.out.println("O hotel não está funcionando.\nFuncionários insuficientes.");
+        }
     }
 }
