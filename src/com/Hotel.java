b9816bf1e6f65569a03a3d81c9186660db31a1fc
jockaplay <jocksonmatheus@gmail.com>
2023-09-20 23:16:07 -0300

    23:15 jockson

--- src/com/Hotel.java
+++ src/com/Hotel.java
@@ -1,8 +1,15 @@
 package com;
+import java.util.ArrayList;
+import com.classes.Employee;
+import com.classes.Cliente;
 
 public class Hotel {
     private static int count = 0;
     public int id = 1;
+
+	//Iniciando uma lista de empregados.
+	ArrayList<Employee> empregados = new ArrayList<Employee>();
+	ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
     public Hotel () {
         id += count;
@@ -11,5 +18,15 @@ public class Hotel {
 
     public int getID() {
         return id;
+    }
+
+    public void addEmployee() {
+        for (int i = 0; i < empregados.size(); i++){
+            
+        }
+    }
+
+    public void addCliente() {
+        
     }
 }
