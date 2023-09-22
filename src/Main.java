eda32cc1e13fc2fec81784178f704434cd59a299
jockaplay <jocksonmatheus@gmail.com>
2023-09-21 23:51:19 -0300

    add methods in hotel

--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,40 @@ public class Main {
 		int start;
         String linha = "========================================================================";
 
-        do {
-			System.out.println(linha);
-			System.out.println("Bem Vindo! Você está entrando ao gerenciador de Hoteis!");
-			System.out.println(linha);
-			System.out.println("Escolha as opções de comando a seguir!");
-			System.out.println(linha);
-            System.out.println("Selecione uma opção:");
-            System.out.println("1. Criar novo hotel");
-            System.out.println("2. Deletar hoteis");
-			System.out.println("3. Ver hoteis");
-            System.out.println("4. Sair");
-			System.out.println("=");
+        // do {
+		// 	System.out.println(linha);
+		// 	System.out.println("Bem Vindo! Você está entrando ao gerenciador de Hoteis!");
+		// 	System.out.println(linha);
+		// 	System.out.println("Escolha as opções de comando a seguir!");
+		// 	System.out.println(linha);
+        //     System.out.println("Selecione uma opção:");
+        //     System.out.println("1. Criar novo hotel");
+        //     System.out.println("2. Deletar hoteis");
+		// 	System.out.println("3. Ver hoteis");
+        //     System.out.println("4. Sair");
+		// 	System.out.println("=");
 
-            start = scanner.nextInt();
+        //     start = scanner.nextInt();
 
-            switch (start) {
-                case 1:
-                    System.out.println("Você irá criar um novo hotel!");
-                    break;
-                case 2:
-                    System.out.println("Você irá deletar um hotel!");
-                    break;
-				case 3:
-					System.out.println("Irá ver todos os hoteis!");
-					break;
-                case 4:
-                    System.out.println("Saindo do programa...\n" + linha);
-                    break;
-					default:
-                    System.out.println("Opção inválida. Tente novamente.");
-                    break;
-            }
+        //     switch (start) {
+        //         case 1:
+        //             System.out.println("Você irá criar um novo hotel!");
+        //             break;
+        //         case 2:
+        //             System.out.println("Você irá deletar um hotel!");
+        //             break;
+		// 		case 3:
+		// 			System.out.println("Irá ver todos os hoteis!");
+		// 			break;
+        //         case 4:
+        //             System.out.println("Saindo do programa...\n" + linha);
+        //             break;
+		// 			default:
+        //             System.out.println("Opção inválida. Tente novamente.");
+        //             break;
+        //     }
 
-        } while (start != 4);
+        // } while (start != 4);
 
 		Hotel h1 = new Hotel();
 		h1.addEmployee();
